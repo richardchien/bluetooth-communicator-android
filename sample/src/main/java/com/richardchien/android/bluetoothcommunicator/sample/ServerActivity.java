1dc1a007025ba17e2cbcbcb951cdcfebeb4a2530
Richard Chien <richardchienthebest@gmail.com>
2016-01-11 21:21:43 +0800

    adjust module dependence

--- sample/src/main/java/com/richardchien/android/bluetoothcommunicator/sample/ServerActivity.java
+++ sample/src/main/java/com/richardchien/android/bluetoothcommunicator/sample/ServerActivity.java
@@ -3,18 +3,18 @@ package com.richardchien.android.bluetoothcommunicator.sample;
 import android.app.AlertDialog;
 import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.richardchien.android.bluetoothcommunicator.BluetoothServer;
-import com.richardchien.android.bluetoothcommunicator.listener.OnReceiveListener;
 import com.richardchien.android.bluetoothcommunicator.listener.OnListenListener;
 import com.richardchien.android.bluetoothcommunicator.listener.OnLoseConnectionListener;
+import com.richardchien.android.bluetoothcommunicator.listener.OnReceiveListener;
 
 import java.util.ArrayList;
 import java.util.List;
